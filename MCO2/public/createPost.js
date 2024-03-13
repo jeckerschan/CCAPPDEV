4036f87bb3c801727431de4230098dd57262acef
Luis <148182285+frostylicious@users.noreply.github.com>
2024-03-14 00:04:25 +0800

    create post update

--- MCO2/public/createPost.js
+++ MCO2/public/createPost.js
@@ -1,46 +1,42 @@
-import { sampleAccounts } from './accountConst.js';
+$(document).ready(function() {
 
-function Post(title, description, tags) {
-    this.title = title;
-    this.desecription = description;
-    this.tags = tags;
-}
+    $("#submit-btn").click(function() {
 
-function addPost(title, description, tags){
-
-}
-
-$(document).ready (function() {
-
-    $("#submit").click(function() {
-
+        /*
         var title = $("#title").val();
         var description = $("#description").val();
         var username = sampleAccounts.getUsername();
         var tags = document.getElementById("CCS");
 
-        var newPost = '<div class="post"' +
-        '<a> href="Post.html"><h3>' + title + '</h3></a>'
-        '<h5>' + username + '</h5>' +
-        '<p>' + description + '</p>' +
-        '<label>Tags:</label>' + '<p>tags</p>' +
-        '<div class="actions">' +
-        '<button class="upvote"><span class="material-symbols-outlined">heart_plus</span></button>' +
-        '<button class="downvote"><span class="material-symbols-outlined">heart_minus</span></button>' +
-        '</div>' +
-        '<div class = "comments">' +
-        '<h4>Comments</h4>' + '<ul>' + '<li>Comment 1</li>' + '</ul>' +
-        '<button class="upvote"><span class="material-symbols-outlined">heart_plus</span></button>' +
-        '<button class="downvote"><span class="material-symbols-outlined">heart_minus</span></button>' +
-        '<button class="reply"><span class="material-symbols-outlined">reply</span></button>' +
-        '</div>' +
-        '<button class="comment"><span class="material-symbols-outlined">add_comment</span></button>' +
-        '</div>';
+        var newPost = '<div class="post">' +
+            '<a href="Post.html"><h3>' + title + '</h3></a>' +
+            '<h5>' + username + '</h5>' +
+            '<p>' + description + '</p>' +
+            '<label>Tags:</label>' + '<p>' + tags + '</p>' +
+            '<div class="actions">' +
+            '<button class="upvote"><span class="material-symbols-outlined">heart_plus</span></button>' +
+            '<button class="downvote"><span class="material-symbols-outlined">heart_minus</span></button>' +
+            '</div>' +
+            '<div class="comments">' +
+            '<h4>Comments</h4>' +
+            '<ul>' +
+            '<li>Comment 1</li>' +
+            '</ul>' +
+            '<button class="upvote"><span class="material-symbols-outlined">heart_plus</span></button>' +
+            '<button class="downvote"><span class="material-symbols-outlined">heart_minus</span></button>' +
+            '<button class="reply"><span class="material-symbols-outlined">reply</span></button>' +
+            '</div>' +
+            '<button class="comment"><span class="material-symbols-outlined">add_comment</span></button>' +
+            '</div>';
 
         $("#postlist-div").append(newPost);
 
+        window.location.href = "MainPage.html";
 
+        */
+
+        window.location.href ="MainPage.html";
     });
-
 });
+
 
