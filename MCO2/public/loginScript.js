bb73eb298db679cfc52bdfe850111b50599f02e2
Disavowedd <128288879+Disavowedd@users.noreply.github.com>
2024-03-13 22:29:15 +0800

    updated these files

--- MCO2/public/loginScript.js
+++ MCO2/public/loginScript.js
@@ -26,8 +26,10 @@ function handleFormSubmit(event) {
 
   
     if (validateLogin(username, password)) {
-      
-        window.location.href = 'MainPage.html';
+
+        saveTempAccount(username, password);
+        //window.location.href = 'MainPage.html';
+        //saveTempAccount(username, password);
     } else {
     
         alert('Invalid username or password. Please try again.');
@@ -36,5 +38,34 @@ function handleFormSubmit(event) {
    
 }
 
+function saveTempAccount(username, password) {
+    // Create the data object to send to the server
+    const data = {
+        username,
+        password
+    };
+
+    // Make a POST request to save the temporary account
+    fetch('http://localhost:3000/saveTempAccount', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (response.ok) {
+            // If the account was saved successfully, redirect to MainPage.html
+            window.location.href = 'MainPage.html';
+        } else {
+            // If there was an error, throw an error and handle it in the catch block
+            throw new Error('Error saving temporary account');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Error saving temporary account. Please try again.');
+    });
+}
 
 document.querySelector('form').addEventListener('submit', handleFormSubmit);
