318833a781f66eb5d9bba746e911c543bf8fb408
jeckerschan <jeck_sanchez@dlsu.edu.ph>
2024-04-03 17:23:29 +0800

    updates

--- MC02LATEST/public/accountConst.js
+++ MC02LATEST/public/accountConst.js
@@ -1,18 +1,19 @@
-function Account(username, password) {
+function Account(username, password, accountID) {
     this.username = username;
     this.password = password;
+    this.accountID = accountID;
 }
 
 const sampleAccounts = [
-    new Account('Gian', 'password1'),
-    new Account('Luis', 'password2'),
-    new Account('Jeck', 'password3'),
-    new Account('TestUser1', 'password4'),
-    new Account('TestUser', 'password5')
+    new Account('Gian', 'password1', '0001'),
+    new Account('Luis', 'password2', '0002'),
+    new Account('Jeck', 'password3', '0003'),
+    new Account('TestUser1', 'password4', '0004'),
+    new Account('TestUser', 'password5', '0005')
 ];
 
-function addAccount(username, password) {
-    const newAccount = new Account(username, password);
+function addAccount(username, password, accountID) {
+    const newAccount = new Account(username, password, accountID);
     sampleAccounts.push(newAccount);
     console.log('New account added:', newAccount.username);
 }
--- MC02LATEST/public/postConst.js
+++ MC02LATEST/public/postConst.js
@@ -1,4 +1,4 @@
-function Post (id, title, description, tags, upvotes, downvotes, comments) {
+function Post (id, title, description, tags, upvotes, downvotes, comments, accountID) {
     this.id = id;
     this.title = title;
     this.description = description;
@@ -6,15 +6,16 @@ function Post (id, title, description, tags, upvotes, downvotes, comments) {
     this.upvotes = upvotes;
     this.downvotes = downvotes;
     this.comments = comments;
+    this.accountID = accountID;
 }
 
 let samplePosts = [
 
-    new Post (0, 'Sinigang', 'Sinigang is a delicious delicacy which originated in the Philippines', ['CCS', 'CLA'], 0, 0, []),
-    new Post (1, 'Adobo', 'I love adobo', ['GCOE', 'COS'], 0, 0, []),
-    new Post (2, 'DLSU', 'De La Salle University is a school', ['COB'], 0, 0, []),
-    new Post (3, 'Hello World', 'Hello there world', ['CLA'], 0, 0, []),
-    new Post (4, 'TITLE', 'Testing description', ['USG'], 0, 0, [])
+    new Post (0, 'Sinigang', 'Sinigang is a delicious delicacy which originated in the Philippines', ['CCS', 'CLA'], 0, 0, [], '0001'),
+    new Post (1, 'Adobo', 'I love adobo', ['GCOE', 'COS'], 0, 0, [], '0001'),
+    new Post (2, 'DLSU', 'De La Salle University is a school', ['COB'], 0, 0, [], '0003'),
+    new Post (3, 'Hello World', 'Hello there world', ['CLA'], 0, 0, [], '0002'),
+    new Post (4, 'TITLE', 'Testing description', ['USG'], 0, 0, [], '0004')
 
 ];
 
